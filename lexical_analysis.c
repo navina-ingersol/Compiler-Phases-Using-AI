393ac423978cab1a0a8949669f8bf14f8587a478
navina-ingersol <navina.ingersol@gmail.com>
2026-01-30 10:40:12 +0530

    Update lexical_analysis.c

--- lexical_analysis.c
+++ lexical_analysis.c
@@ -1,2 +1,17 @@
-KEYWORD
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
+int main() {
+    char input[100];
+    scanf("%s", input);
+
+    if (strcmp(input, "int") == 0)
+        printf("KEYWORD");
+    else if (isdigit(input[0]))
+        printf("NUMBER");
+    else
+        printf("IDENTIFIER");
+
+    return 0;
+}
